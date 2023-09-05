c7795a2eeed3860e198f7a74b5d780f0d5f423c1
Ryan Stewart <ryanstew17@gmail.com>
2023-09-05 13:05:56 -0300

    Integration Testing

--- src/hooks/tests/Application.test.js
+++ src/hooks/tests/Application.test.js
@@ -1,11 +1,30 @@
 import React from "react";
 import Application from "components/Application";
-import { render, waitForElement } from "@testing-library/react";
+import { render, waitForElement, prettyDOM, fireEvent, getByAltText, getByPlaceholderText, getByText, getAllByTestId } from "@testing-library/react";
 
 describe("Appointment", () => {
   it("defaults to Monday and changes the schedule when a new day is selected", () => {
     const { getByText } = render(<Application />);
-  
     return waitForElement(() => getByText("Monday"));
+  });
+
+  it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
+    const { container } = render(<Application />);
+  
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+  
+    const appointments = getAllByTestId(container, "appointment");
+    const appointment = appointments[0];
+  
+    fireEvent.click(getByAltText(appointment, "Add"));
+  
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+  
+    fireEvent.click(getByText(appointment, "Save"));
+  
+    console.log(prettyDOM(appointment));
   });
 });
